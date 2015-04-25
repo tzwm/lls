2e91692fad578f03fa455b0cc302fa9338f3a6f3
tzwm <wangjunzhe4444@gmail.com>
2015-04-25 21:37:18 +0800

    Add online time display for unlogined user by JavaScript

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function() {
+  var visited_time = $.now();
+
+  $(window).unload(function() {
+    var now = $.now();
+  });
+
+
+  var setOnlineTime = function() {
+    var container = $('#online-time');
+    var onlineTime = ($.now() - visited_time) / 1000 / 60;
+    container.text(Math.floor(onlineTime));
+  };
+
+  setInterval(setOnlineTime, 1000);
+
+});
